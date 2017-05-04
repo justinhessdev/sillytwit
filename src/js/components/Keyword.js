d841c397180d6b04fd6aae9b3a7d6cc0366dac27
justinhessdev <justinhessdev@gmail.com>
2017-05-04 18:22:43 -0400

    linter changes

--- src/js/components/Keyword.js
+++ src/js/components/Keyword.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
 
-export default function Keyword(props){
-
+export default function Keyword(props) {
   const divStyle = {
-    marginLeft: '20px'
-  }
+    marginLeft: '20px',
+  };
 
   return (
     <div style={divStyle} id="componentKeyword">
       <h6>Filter by keyword</h6>
       <label className="sr-only" htmlFor="inputKeyword">keyword</label>
-      <input className="form-control" id="inputKeyword" type='text' onBlur={props.onBlur} placeholder="enter keyword"/>
+      <input
+        className="form-control" id="inputKeyword" type="text"
+        onBlur={props.onBlur} placeholder="enter keyword"
+      />
     </div>
-  )
+  );
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,28 +1,28 @@
-import React from "react"
+import React from 'react';
 
-import Keyword from "./Keyword"
-import Rating from "./Rating"
-import ReviewList from "./ReviewList"
-import Total from "./Total"
-import LoadMore from "./LoadMore"
+import Keyword from './Keyword';
+import Rating from './Rating';
+import ReviewList from './ReviewList';
+import Total from './Total';
+import LoadMore from './LoadMore';
 
 export default class Layout extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       reviews: [],
       total: 0,
       pages: 1,
       url: process.env.REACT_APP_APPFIGURES_URL,
       keyword: '',
-      rating: '' // value of ratings drop down
-    }
+      rating: '', // value of ratings drop down
+    };
 
     this.setReviews = this.setReviews.bind(this);
     this.handleLoadMore = this.handleLoadMore.bind(this);
-    this.onBlur = this.onBlur.bind(this)
-    this.handleRating = this.handleRating.bind(this)
+    this.onBlur = this.onBlur.bind(this);
+    this.handleRating = this.handleRating.bind(this);
   }
 
 /*
@@ -31,116 +31,133 @@ loading by default the first 25 reviews
 
 process.env.REACT_APP_APPFIGURES_URL --- hiding the url
 */
-  componentWillMount() {
 
-      var self = this
-      const fetchReviews = fetch(this.state.url, {credentials: 'same-origin'})
+  /* global fetch*/
+  componentWillMount() {
+    const self = this;
+    const fetchReviews = fetch(this.state.url, { credentials: 'same-origin' });
+
+    function loadMyReviews(data) {
+      data.json().then((jsonData) => {
+        self.setReviews(jsonData);
+      });
+    }
+
+    fetchReviews.then(loadMyReviews);
+  }
+
+  onBlur(event) {
+    const self = this;
+    const keyword = event.target.value;
+
+    function updateURLWithKeyword() {
+      // console.log("new keyword state is " + self.state.keyword);
+      const url = this.generateURL(
+        self.state.url,
+        self.state.pages,
+        self.state.keyword,
+        self.state.rating,
+      );
+      // console.log("filtered url is: " + url);
+      const fetchReviews = fetch(url, { credentials: 'same-origin' });
 
       function loadMyReviews(data) {
         data.json().then((jsonData) => {
-          self.setReviews(jsonData)
-        })
+          self.resetReviews(jsonData);
+        });
       }
 
-      fetchReviews.then(loadMyReviews)
+      fetchReviews.then(loadMyReviews);
     }
 
-    setReviews(data) {
-      this.setState({
-        reviews: this.state.reviews.concat(data.reviews),
-        total: data.total
-      })
+    this.setState({
+      keyword,
+      pages: 1,
+    }, () => updateURLWithKeyword());
+  }
+
+  setReviews(data) {
+    this.setState({
+      reviews: this.state.reviews.concat(data.reviews),
+      total: data.total,
+    });
+  }
+
+  generateURL(host, pages, keyword, rating) {
+    let url = '';
+    if (keyword === '' && rating === '') {
+      url = `${host}?page= ${pages}`;
+    } else if (keyword === '') {
+      url = `${host}?page=${pages}&stars${rating}`;
+    } else if (rating === '') {
+      url = `${host}?page=${pages}&q=${keyword}`;
+    } else {
+      url = `${host}?page=${pages}&q=${keyword}&stars=${rating}`;
     }
 
-    resetReviews(data) {
-      this.setState({
-        reviews: data.reviews,
-        total: data.total
-      })
-    }
+    return url;
+  }
 
-    handleLoadMore() {
-      var self = this
+  resetReviews(data) {
+    this.setState({
+      reviews: data.reviews,
+      total: data.total,
+    });
+  }
 
-      this.setState({
-        pages: this.state.pages+1
-      }, () => loadPaginatedMessages())
+  handleLoadMore() {
+    const self = this;
 
-      function loadPaginatedMessages() {
-        var url = self.generateURL(self.state.url, self.state.pages, self.state.keyword, self.state.rating)
+    function loadPaginatedMessages() {
+      const url = self.generateURL(
+        self.state.url,
+        self.state.pages,
+        self.state.keyword,
+        self.state.rating,
+      );
+      const fetchReviews = fetch(url, { credentials: 'same-origin' });
 
-        const fetchReviews = fetch(url, {credentials: 'same-origin'})
-
-        function loadMyReviews(data) {
-          data.json().then((jsonData) => {
-            self.setReviews(jsonData)
-          })
-        }
-
-        fetchReviews.then(loadMyReviews)
+      function loadMyReviews(data) {
+        data.json().then((jsonData) => {
+          self.setReviews(jsonData);
+        });
       }
+
+      fetchReviews.then(loadMyReviews);
     }
 
-    generateURL(host, pages, keyword, rating) {
-      var url
-      if(keyword=='' && rating ==''){
-        url = host+'?page='+pages
-      } else if(keyword=='') {
-        url = host+'?page='+pages+'&stars='+rating
-      } else if(rating==''){
-        url = host+'?page='+pages+'&q='+keyword
-      } else {
-       url = host+'?page='+pages+'&q='+keyword+'&stars='+rating
-     }
+    this.setState({
+      pages: this.state.pages + 1,
+    }, () => loadPaginatedMessages());
+  }
 
-     return url
-    }
+  handleRating(evt) {
+    const self = this;
+    const rating = evt.target.value;
 
-    onBlur(event) {
-      var self = this
-      var keyword = event.target.value
-      this.setState({
-        keyword: keyword,
-        pages: 1
-      }, ()=> updateURLWithKeyword())
+    function updateURLWithRating() {
+      const url = self.generateURL(
+        self.state.url,
+        self.state.pages,
+        self.state.keyword,
+        self.state.rating,
+      );
+      const fetchReviews = fetch(url, { credentials: 'same-origin' });
 
-      function updateURLWithKeyword() {
-        console.log("new keyword state is " + self.state.keyword);
-        var url = self.generateURL(self.state.url, self.state.pages, self.state.keyword, self.state.rating)
-        console.log("filtered url is: " + url);
-        const fetchReviews = fetch(url, {credentials: 'same-origin'})
-
-        function loadMyReviews(data) {
-          data.json().then((jsonData) => {
-            self.resetReviews(jsonData)
-          })
-        }
-
-        fetchReviews.then(loadMyReviews)
+      function loadMyReviews(data) {
+        data.json().then((jsonData) => {
+          self.resetReviews(jsonData);
+        });
       }
+
+      fetchReviews.then(loadMyReviews);
     }
 
-    handleRating(evt) {
-      var self = this
-      var rating = evt.target.value
-      this.setState({
-        rating: rating,
-        pages: 1
-      }, ()=> updateURLWithRating())
-
-      function updateURLWithRating() {
-        var url = self.generateURL(self.state.url, self.state.pages, self.state.keyword, self.state.rating)
-        const fetchReviews = fetch(url, {credentials: 'same-origin'})
-
-        function loadMyReviews(data) {
-          data.json().then((jsonData) => {
-            self.resetReviews(jsonData)
-          })
-        }
-
-        fetchReviews.then(loadMyReviews)
-      }
-    }
+    this.setState({
+      rating,
+      pages: 1,
+    }, () => updateURLWithRating());
+  }
 
 /*
 Renders Keyword ad Rating components so far
@@ -151,24 +168,24 @@ Bootstrap design
       <div id="componentLayout">
         <div className="row">
           <div className="col-xs-8">
-            <Keyword onBlur={this.onBlur}/>
+            <Keyword onBlur={this.onBlur} />
           </div>
           <div className="col-xs-2">
-            <Rating rating={this.state.rating} handleRating={this.handleRating}/>
+            <Rating rating={this.state.rating} handleRating={this.handleRating} />
           </div>
         </div>
         <div className="row">
           <div className="col-xs-10">
-            <Total total={this.state.total}/>
-            <ReviewList reviews={this.state.reviews}/>
+            <Total total={this.state.total} />
+            <ReviewList reviews={this.state.reviews} />
           </div>
         </div>
         <div className="row">
           <div className="col-xs-10">
-            <LoadMore handleLoadMore={this.handleLoadMore} peach="fuzz"/>
+            <LoadMore handleLoadMore={this.handleLoadMore} peach="fuzz" />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
