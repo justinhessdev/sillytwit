27bd1a43f8eee5abd0e9d73a08e91832ece7981c
justinhessdev <justinhessdev@gmail.com>
2017-05-16 11:42:46 -0400

    updated review list

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -12,6 +12,7 @@ export default class Layout extends React.Component {
     super(props);
     this.state = {
       reviews: [],
+      reviewTypeList: [],
       total: 0,
       pages: 1,
       url: process.env.REACT_APP_APPFIGURES_URL,
@@ -19,7 +20,6 @@ export default class Layout extends React.Component {
       rating: '',
     };
 
-    this.setReviews = this.setReviews.bind(this);
     this.handleLoadMore = this.handleLoadMore.bind(this);
     this.onBlur = this.onBlur.bind(this);
     this.handleRating = this.handleRating.bind(this);
@@ -33,18 +33,22 @@ process.env.REACT_APP_APPFIGURES_URL --- hiding the url
 */
 
   /* global fetch*/
-  componentWillMount() {
+  componentDidMount() {
     const self = this;
-    console.log(process.env.REACT_APP_APPFIGURES_URL);
-    const fetchReviews = fetch(this.state.url, { credentials: 'same-origin' });
 
     function loadMyReviews(data) {
-      data.json().then((jsonData) => {
-        self.setReviews(jsonData);
-      });
+      data.json()
+        .then((jsonData) => {
+          self.setState({
+            reviews: self.state.reviews.concat(jsonData.reviews),
+            total: jsonData.total,
+          });
+
+          self.sortReviews();
+        });
     }
 
-    fetchReviews.then(loadMyReviews);
+    fetch(this.state.url, { credentials: 'same-origin' }).then(loadMyReviews);
   }
 
   onBlur(event) {
@@ -62,7 +66,12 @@ process.env.REACT_APP_APPFIGURES_URL --- hiding the url
 
       function loadMyReviews(data) {
         data.json().then((jsonData) => {
-          self.resetReviews(jsonData);
+          self.setState({
+            reviews: jsonData.reviews,
+            total: jsonData.total,
+          });
+
+          self.sortReviews();
         });
       }
 
@@ -75,11 +84,76 @@ process.env.REACT_APP_APPFIGURES_URL --- hiding the url
     }, () => updateURLWithKeyword());
   }
 
-  setReviews(data) {
-    this.setState({
-      reviews: this.state.reviews.concat(data.reviews),
-      total: data.total,
+  sortReviews() {
+    console.log("this is: ");
+    console.log(this);
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentYear = currentDate.getFullYear();
+    const currentDayofMonth = currentDate.getDate();
+    const reviewTypeList = {};
+    console.log(this.state);
+    this.state.reviews.forEach((review) => {
+      const date = review.date.substring(0, 10).replace(/-/g, '\/');
+      const reviewDate = new Date(date);
+      const reviewMonth = reviewDate.getMonth() + 1;
+      const reviewYear = reviewDate.getFullYear();
+      const reviewDayOfMonth = reviewDate.getDate();
+      // const reviewDayOfWeek = reviewDate.getDay();
+
+       // TODAY
+      if (currentDayofMonth === reviewDayOfMonth &&
+        currentMonth === reviewMonth &&
+        currentYear === reviewYear) {
+        if (!('Today' in reviewTypeList)) {
+          reviewTypeList.Today = [];
+        }
+
+        reviewTypeList.Today.push(review);
+        return;
+      }
+
+      // YESTERDAY
+      if (currentDayofMonth === reviewDayOfMonth + 1
+         && currentMonth === reviewMonth &&
+         currentYear === reviewYear) {
+        if (!('Yesterday' in reviewTypeList)) {
+          reviewTypeList.Yesterday = [];
+        }
+
+        reviewTypeList.Yesterday.push(review);
+        return;
+      }
+
+       // THIS MONTH
+      if (currentMonth === reviewMonth && currentYear === reviewYear) {
+        if (!('This Month' in reviewTypeList)) {
+          reviewTypeList['This Month'] = [];
+        }
+
+        reviewTypeList['This Month'].push(review);
+        return;
+      }
+
+       // LAST MONTH
+      if (currentMonth === reviewMonth + 1 && currentYear === reviewYear) {
+        if (!('Last Month' in reviewTypeList)) {
+          reviewTypeList['Last Month'] = [];
+        }
+
+        reviewTypeList['Last Month'].push(review);
+        return;
+      }
+
+       // ALL OTHER MONTH / YEAR COMBINATIONS
+      if (!((`Month ${reviewMonth}, ${reviewYear}`) in reviewTypeList)) {
+        reviewTypeList[`Month ${reviewMonth}, ${reviewYear}`] = [];
+      }
+
+      reviewTypeList[`Month ${reviewMonth}, ${reviewYear}`].push(review);
     });
+
+    this.setState({ reviewTypeList });
   }
 
   generateURL(host, pages, keyword, rating) {
@@ -97,13 +171,6 @@ process.env.REACT_APP_APPFIGURES_URL --- hiding the url
     return url;
   }
 
-  resetReviews(data) {
-    this.setState({
-      reviews: data.reviews,
-      total: data.total,
-    });
-  }
-
   handleLoadMore() {
     const self = this;
     function loadPaginatedMessages() {
@@ -117,7 +184,12 @@ process.env.REACT_APP_APPFIGURES_URL --- hiding the url
 
       function loadMyReviews(data) {
         data.json().then((jsonData) => {
-          self.setReviews(jsonData);
+          self.setState({
+            reviews: self.state.reviews.concat(jsonData.reviews),
+            total: jsonData.total,
+          });
+
+          self.sortReviews();
         });
       }
 
@@ -143,7 +215,12 @@ process.env.REACT_APP_APPFIGURES_URL --- hiding the url
 
       function loadMyReviews(data) {
         data.json().then((jsonData) => {
-          self.resetReviews(jsonData);
+          self.setState({
+            reviews: jsonData.reviews,
+            total: jsonData.total,
+          });
+
+          self.sortReviews();
         });
       }
 
@@ -174,7 +251,7 @@ Bootstrap design
         <div className="row">
           <div className="col-xs-10">
             <Total total={this.state.total} />
-            <ReviewList reviews={this.state.reviews} />
+            <ReviewList reviewTypeList={this.state.reviewTypeList} reviews={this.state.reviews} />
           </div>
         </div>
         <div className="row">
--- src/js/components/ReviewList.js
+++ src/js/components/ReviewList.js
@@ -1,140 +1,48 @@
 import React from "react"
 
-export default class ReviewList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      reviewTypeList: {},
-    };
-  }
+export default function ReviewList(props) {
+  const headerStyle = {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    background: '#dddddd',
+    border: '1px solid black',
+    padding: '10px',
+  };
 
-  componentWillReceiveProps(nextProps) {
-    // console.log("in comopnentwillreceiveprops in reviewlist");
-    // console.log(nextProps);
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1;
-    const currentYear = currentDate.getFullYear();
-    const currentDayofMonth = currentDate.getDate();
-    const reviewTypeList = {};
-    nextProps.reviews.forEach((review) => {
-      const date = review.date.substring(0, 10).replace(/-/g, '\/');
-      const reviewDate = new Date(date);
-      const reviewMonth = reviewDate.getMonth() + 1;
-      const reviewYear = reviewDate.getFullYear();
-      const reviewDayOfMonth = reviewDate.getDate();
-      // const reviewDayOfWeek = reviewDate.getDay();
+  const divStyle = {
+    marginLeft: '20px',
+    marginBottom: '20px',
+    marginTop: '20px',
+    height: '450px',
+    overflow: 'auto',
+  };
 
-       // TODAY
-      if (currentDayofMonth === reviewDayOfMonth &&
-        currentMonth === reviewMonth &&
-        currentYear === reviewYear) {
-        if (!('Today' in reviewTypeList)) {
-          reviewTypeList.Today = [];
-        }
-
-        reviewTypeList.Today.push(review);
-        return;
-      }
-
-      // YESTERDAY
-      if (currentDayofMonth === reviewDayOfMonth + 1
-         && currentMonth === reviewMonth &&
-         currentYear === reviewYear) {
-        if (!('Yesterday' in reviewTypeList)) {
-          reviewTypeList.Yesterday = [];
-        }
-
-        reviewTypeList.Yesterday.push(review);
-        return;
-      }
-
-      // THIS WEEK
-      //  if(currentDayofWeek != 1 && currentDayofWeek != 2
-      // && currentMonth == reviewMonth && currentYear == reviewYear
-      //  && mess) {
-      //    if(!("This Week" in reviewTypeList)){
-      //      reviewTypeList["This Week"] = []
-      //    }
-      //
-      //    reviewTypeList["This Week"].push(review)
-      //    return
-      //  }
-
-       // THIS MONTH
-      if (currentMonth === reviewMonth && currentYear === reviewYear) {
-        if (!('This Month' in reviewTypeList)) {
-          reviewTypeList['This Month'] = [];
-        }
-
-        reviewTypeList['This Month'].push(review);
-        return;
-      }
-
-       // LAST MONTH
-      if (currentMonth === reviewMonth + 1 && currentYear === reviewYear) {
-        if (!('Last Month' in reviewTypeList)) {
-          reviewTypeList['Last Month'] = [];
-        }
-
-        reviewTypeList['Last Month'].push(review);
-        return;
-      }
-
-       // ALL OTHER MONTH / YEAR COMBINATIONS
-      if (!((`Month ${reviewMonth}, ${reviewYear}`) in reviewTypeList)) {
-        reviewTypeList[`Month ${reviewMonth}, ${reviewYear}`] = [];
-      }
-
-      reviewTypeList[`Month ${reviewMonth}, ${reviewYear}`].push(review);
-    });
-
-    this.setState({ reviewTypeList });
-  }
-
-  render() {
-    const headerStyle = {
-      fontWeight: 'bold',
-      textAlign: 'center',
-      background: '#dddddd',
-      border: '1px solid black',
-      padding: '10px',
-    };
-
-    const divStyle = {
-      marginLeft: '20px',
-      marginBottom: '20px',
-      marginTop: '20px',
-      height: '450px',
-      overflow: 'auto',
-    };
-
-    return (
-      <div style={divStyle} id="reviewList">
-        {Object.keys(this.state.reviewTypeList).map((key) => {
-          return (
-            <div key={key}>
-              <h2 style={headerStyle}>{key}</h2>
-              <ul className="list-group">
-                {this.state.reviewTypeList[key].map((review) => {
-                  const date = review.date.substring(0, 10);
-                  const time = review.date.slice(12);
-                  return (
-                    <li className="list-group-item" key={review.id}>
-                      <span className="bold">Author:</span> {review.author} <br></br>
-                      <span className="bold">Date:</span> {date} <br></br>
-                      <span className="bold">Time:</span> {time} <br></br>
-                      <span className="bold">Title:</span> {review.title} <br></br>
-                      <span className="bold">Review:</span> {review.review} <br></br>
-                      <span className="bold">Rating:</span> {review.stars} <br></br>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          );
-        })
-      }
-      </div>
-    );
-  }
+  return (
+    <div style={divStyle} id="reviewList">
+      {Object.keys(props.reviewTypeList).map((key) => {
+        return (
+          <div key={key}>
+            <h2 style={headerStyle}>{key}</h2>
+            <ul className="list-group">
+              {props.reviewTypeList[key].map((review) => {
+                const date = review.date.substring(0, 10);
+                const time = review.date.slice(12);
+                return (
+                  <li className="list-group-item" key={review.id}>
+                    <span className="bold">Author:</span> {review.author} <br></br>
+                    <span className="bold">Date:</span> {date} <br></br>
+                    <span className="bold">Time:</span> {time} <br></br>
+                    <span className="bold">Title:</span> {review.title} <br></br>
+                    <span className="bold">Review:</span> {review.review} <br></br>
+                    <span className="bold">Rating:</span> {review.stars} <br></br>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })
+    }
+    </div>
+  );
 }
