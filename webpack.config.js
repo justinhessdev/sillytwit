09d96f3ff0f7355d68e4c2fc488497613a914624
Justin Hess <jhess@JHess-MBP1.fios-router.home>
2017-05-29 18:26:05 -0400

    updated webpack config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const debug = process.env.NODE_ENV !== "production";
 const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
-const plugins = [];
+const denv = () => new Dotenv({ path: './.env', safe: false })
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -22,10 +23,17 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new Dotenv({
-      path: './.env',
-      safe: false,
-    }),
-  ],
+  plugins: debug ? [
+    denv(),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+  ] : [
+      denv(),
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ],
 };
+
+console.log('logging plugins: We should see process.env.REACT_APP_APPFIGURES_URL, process.env.NODE_ENV, process.env.FLAG');
+console.log(module.exports.plugins);
