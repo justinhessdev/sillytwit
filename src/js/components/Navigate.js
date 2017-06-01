0933736daf0b7871a19e569b5a000d1489f6796e
Justin Hess <jhess@JHess-MBP1.fios-router.home>
2017-06-01 15:36:34 -0400

    updated nav

--- src/js/components/Navigate.js
+++ src/js/components/Navigate.js
@@ -4,13 +4,20 @@ import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 're
 class Navigate extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Container}>
-          <IndexRoute component={Home} />
-          <Route path='/address' component={Address} />
-          <Route path='*' component={NotFound} />
-        </Route>
-      </Router>
+      <div>
+        <Container>
+          <Home />
+          <Address />
+          <NotFound />
+        </Container>
+      </div>
+      // <Router history={hashHistory}>
+      //   <Route path='/' component={Container}>
+      //     <IndexRoute component={Home} />
+      //     <Route path='/address' component={Address} />
+      //     <Route path='*' component={NotFound} />
+      //   </Route>
+      // </Router>
     );
   }
 }
@@ -18,17 +25,15 @@ class Navigate extends Component {
 const Home = () => <h1>Hello from Home!</h1>;
 const Address = () => <h1>We are located at Somewhere Over The Rainbow</h1>;
 const NotFound = () => <h1>404.. This page is not found!</h1>;
-const Nav = () => (
-  <div>
-    <Link to='/'>Home</Link>&nbsp;
-    <Link to='/address'>Address</Link>
-  </div>
-);
 
 const Container = (props) => (
   <div>
-    <Nav />
-    {props.children}
+    {
+      React.Children.map(props.children, (child, i) => {
+        if (i > 1) return
+        return child;
+      })
+    }
   </div>
 );
 
