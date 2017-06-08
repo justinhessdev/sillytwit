ce76b05f302a55fd0b8dde632725ba4bdac89b6c
jhess <juhess88@gmail.com>
2017-06-07 21:00:00 -0400

    playing around with cloning elements

--- src/js/components/Navigate.js
+++ src/js/components/Navigate.js
@@ -4,13 +4,11 @@ import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 're
 class Navigate extends Component {
   render() {
     return (
-      <div>
-        <Container>
-          <Home />
+        <Container contains={'who knows'}>
+          <Home location={'El Salvador'} />
           <Address />
           <NotFound />
         </Container>
-      </div>
       // <Router history={hashHistory}>
       //   <Route path='/' component={Container}>
       //     <IndexRoute component={Home} />
@@ -24,15 +22,20 @@ class Navigate extends Component {
 
 const Home = () => <h1>Hello from Home!</h1>;
 const Address = () => <h1>We are located at Somewhere Over The Rainbow</h1>;
-const NotFound = () => <h1>404.. This page is not found!</h1>;
+const NotFound = () => <h1>404.. This page is not found -- JK it is!</h1>;
 
+/*
+Using clone element now all children of Container --- Home, Address, and NotFound
+Receive props hmmmm = "who knows" 
+*/
 const Container = (props) => (
   <div>
     {
-      React.Children.map(props.children, (child, i) => {
-        if (i > 2) return
-        return child;
-      })
+      React.Children.map(props.children, (child, i) =>
+        React.cloneElement(child, {
+          hmmmmm: props.contains
+        })
+      )
     }
   </div>
 );
